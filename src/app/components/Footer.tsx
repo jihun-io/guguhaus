6aded78c3bd460c433a86412c1961384a51def27
김지훈 <kim@jihun.io>
2025-03-02 01:42:32 +0900

    푸터 컴포넌트 추가 및 저작권 정보 표시

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-8">
+      <p className="text-sm">Copyright © 2025 Jun Park. All rights reserved.</p>
+    </footer>
+  );
+}
