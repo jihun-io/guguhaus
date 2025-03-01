793d1024e19d0faacc1e17f076b788e4c8e2c79a
김지훈 <kim@jihun.io>
2025-03-02 01:42:39 +0900

    레이아웃에 푸터 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Header />
         <main className="flex flex-col py-16 min-h-full">{children}</main>
+        <Footer />
       </body>
     </html>
   );
