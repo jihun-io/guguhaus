2f308e1a923138fb306181fd427c196c123f7781
김지훈 <kim@jihun.io>
2025-03-02 01:46:44 +0900

    연락 페이지 컴포넌트 추가

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,18 @@
+import { MainTitle } from "../components/Title";
+
+export default function Contact() {
+  return (
+    <>
+      <MainTitle title="CONTACT" subtitle="연락" />
+      <article className="text-center h-full flex flex-col justify-center items-center">
+        <a href="mailto:saeparandol@naver.com" className="block my-8">
+          saeparandol@naver.com
+        </a>
+        <p>특별한 문의 사항이 있을 시 위의 이메일로 연락 부탁드리겠습니다.</p>
+        <p className="uppercase">
+          Please contact E-mail above for any questions
+        </p>
+      </article>
+    </>
+  );
+}
