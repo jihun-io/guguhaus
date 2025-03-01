9db9164f5534b09879d27d48da92470756e3ddc3
김지훈 <kim@jihun.io>
2025-03-02 01:42:16 +0900

    메인 페이지에 Work In Progress, Articles, History 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,105 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Articles, History, WorkInProgress } from "./components/Cards";
+
+const wipData = [
+  {
+    title: "THUMP!",
+    genre: "다큐멘터리",
+    year: "@2025",
+    href: "/works/1",
+    image: "/images/sample1.jpg",
+    imageAlt: "sample 1",
+    createdAt: "2021-10-01",
+    updatedAt: "2021-10-01",
+  },
+  {
+    title: "THUMP!",
+    genre: "다큐멘터리",
+    year: "@2025",
+    href: "/works/1",
+    image: "/images/sample1.jpg",
+    imageAlt: "sample 1",
+    createdAt: "2021-10-01",
+    updatedAt: "2021-10-01",
+  },
+];
+
+const articlesData = [
+  {
+    title: "유령서점",
+    category: "InEar",
+    desc: "한 번 잠잠했던 이들이 수면 위로 빼꼼히 올라오면",
+    image: "/images/sample2.jpg",
+    imageAlt: "sample 2",
+    href: "works/3",
+    createdAt: "2021-10-01",
+    updatedAt: "2021-10-01",
+  },
+  {
+    title: "ZIN CHOI",
+    category: "InEar",
+    desc: "마냥 귀엽게만 보지 말아주세요",
+    image: "/images/sample3.jpg",
+    imageAlt: "sample 3",
+    href: "works/3",
+    createdAt: "2021-10-01",
+    updatedAt: "2021-10-01",
+  },
+  {
+    title: "아무도 없는 숲속에서",
+    category: "InSeat",
+    desc: "지나가는 개구리가 반드시 듣고야 마는 그 소리",
+    image: "/images/sample4.jpg",
+    imageAlt: "sample 4",
+    href: "works/3",
+    createdAt: "2021-10-01",
+    updatedAt: "2021-10-01",
+  },
+];
+
+const historyData = [
+  {
+    title: "혹성의 아이",
+    titleEng: "A Child From Green Planet",
+    category: "단편, 영화, 패키지 구성",
+    date: "2025-02-01",
+    image: "/images/sample1.jpg",
+    imageAlt: "sample 5",
+    href: "/works/1",
+    createdAt: "2025-03-02",
+    updatedAt: "2025-03-02",
+  },
+  {
+    title: "100% 유해한 그것",
+    titleEng: "100% NOXIOUS THING",
+    category: "단편, 영화, 패키지 구성",
+    date: "2021-07-01",
+    image: "/images/sample2.jpg",
+    imageAlt: "sample 6",
+    href: "/works/1",
+    createdAt: "2025-03-02",
+    updatedAt: "2025-03-02",
+  },
+  {
+    title: "혹성의 아이 UNPLUGGED",
+    titleEng: "A Child From Green Planet UNPLUGGED",
+    category: "도서, 출판",
+    date: "2025-02-01",
+    image: "/images/sample3.jpg",
+    imageAlt: "sample 7",
+    href: "/works/1",
+    createdAt: "2025-03-02",
+    updatedAt: "2025-03-02",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <WorkInProgress data={wipData} />
+      <Articles data={articlesData} />
+      <History data={historyData} />
+    </>
   );
 }
