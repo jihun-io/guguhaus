0c8bc919adbfaba5d6111c9687357b2090bedd5c
김지훈 <kim@jihun.io>
2025-03-03 00:26:54 +0900

    Google Cloud Storage 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "storage.googleapis.com", // Google Cloud Storage 도메인 추가
+    ],
+  },
+};
 
 export default nextConfig;
