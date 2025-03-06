c6b4b807d37f7b5abce18592d3a41827fd62e756
김지훈 <kim@jihun.io>
2025-03-06 22:07:37 +0900

    Notion API를 사용하여 데이터 가져오기 로직으로 변경 및 불필요한 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,23 @@
 import { Articles, History, WorkInProgress } from "@/components/Cards";
 import { adminDb } from "./lib/firebaseAdmin";
-
-interface Post {
-  id: string;
-  title: string;
-  postId: string;
-  category: string;
-  createdAt: string;
-  updatedAt: string;
-  image: string;
-  imageAlt: string;
-  // 기타 필드
-  date?: string;
-  genre?: string;
-  articleCategory?: string;
-  historyCategory?: string;
-  desc?: string;
-  titleEng?: string;
-}
+import {
+  getArticlesData,
+  getData,
+  getHistoryData,
+  getWipData,
+  notionDatabase,
+} from "@/lib/notion";
 
 export default async function Home() {
-  const wipData: Post[] = [];
-  const articlesData: Post[] = [];
-  const historyData: Post[] = [];
-
-  const totalData = await adminDb
-    .collection("posts")
-    .get()
-    .then((snapshot) => {
-      return snapshot.docs
-        .filter((doc) => doc.data().isDeleted === false)
-        .map((doc) => {
-          const data = doc.data();
-
-          return {
-            id: doc.id,
-            title: data.title,
-            category: data.category,
-            desc: data.desc,
-            genre: data.genre,
-            articleCategory: data.articleCategory,
-            historyCategory: data.historyCategory,
-            titleEng: data.titleEng,
-            image: data.imageUrl,
-            imageAlt: data.imageAlt,
-            postId: data.postId,
-            createdAt: data.createdAt,
-            updatedAt: data.updatedAt,
-            content: undefined,
-          };
-        });
-    });
-
-  totalData.forEach((item) => {
-    if (item.category === "Work In Progress") {
-      wipData.push(item);
-    } else if (item.category === "Articles") {
-      articlesData.push(item);
-    } else if (item.category === "History") {
-      historyData.push(item);
-    }
-  });
+  const newWipData = await getWipData();
+  const newArticlesData = await getArticlesData();
+  const newHistoryData = await getHistoryData();
 
   return (
     <>
-      <WorkInProgress data={wipData} />
-      <Articles data={articlesData} />
-      <History data={historyData} />
+      <WorkInProgress data={newWipData} />
+      <Articles data={newArticlesData} />
+      <History data={newHistoryData} />
     </>
   );
 }
--- src/app/participants/page.tsx
+++ src/app/participants/page.tsx
@@ -1,46 +1,20 @@
 import { Participants } from "@/components/Cards";
 import { MainTitle } from "@/components/Title";
 import { adminDb } from "@/app/lib/firebaseAdmin";
+import { getParticipantsData } from "@/lib/notion";
 
 export const metadata = {
   title: "PARTICIPANTS - 99haus",
   description: "프로젝트 참여자들을 소개합니다.",
 };
 
-interface Artist {
-  image: string;
-  imageAlt: string;
-  artist: string;
-  job: string;
-  href: string;
-  social: string;
-}
-
 export default async function ParticipantsPage() {
-  const artistData = await adminDb
-    .collection("participants")
-    .orderBy("order")
-    .get()
-    .then((snapshot) => {
-      return snapshot.docs.map((doc) => {
-        const data = doc.data();
-
-        return {
-          id: doc.id,
-          artist: data.artist,
-          job: data.job,
-          social: data.social,
-          href: data.href,
-          image: data.imageUrl,
-          imageAlt: data.imageAlt,
-        } as Artist;
-      });
-    });
+  const newArtistsData = await getParticipantsData();
 
   return (
     <>
       <MainTitle title="PARTICIPANTS" subtitle="프로젝트 참여자들" />
-      <Participants artists={artistData} />
+      <Participants artists={newArtistsData} />
     </>
   );
 }
