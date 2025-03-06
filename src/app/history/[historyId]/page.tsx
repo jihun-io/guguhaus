ebb59e4a29efc3fc8097a804b08d62334e768c30
김지훈 <kim@jihun.io>
2025-03-07 02:48:21 +0900

    새로운 HistoryPage 컴포넌트 추가: History 콘텐츠를 렌더링하고 Notion API에서 데이터 가져오기

---
+++ src/app/history/[historyId]/page.tsx
@@ -0,0 +1,45 @@
+import PostArticle from "@/components/Posts";
+import { getContent } from "@/lib/notion";
+import Image from "next/image";
+import React from "react";
+
+export default async function HistoryPage({
+  params,
+}: {
+  params: { historyId: string };
+}) {
+  const { historyId } = params;
+  const historyContent = await getContent({
+    category: "history",
+    id: historyId,
+  });
+
+  if (!historyContent || !historyContent.htmlContent) {
+    return (
+      <article className="h-full flex justify-center items-center">
+        <p>해당 포스트를 찾을 수 없습니다.</p>
+      </article>
+    );
+  }
+
+  return (
+    <section>
+      <Image
+        src={historyContent.properties.thumbnail}
+        alt={historyContent.properties.imageAlt}
+        width={480}
+        height={480}
+        className="aspect-[210/297] object-cover w-full max-w-[480px] my-2"
+      />
+      <h2 className="text-2xl">{historyContent.properties.title}</h2>
+      <p>{historyContent.properties.titleEng}</p>
+      <p>
+        {historyContent.properties.historyCategory},{" "}
+        {historyContent.properties.year}
+      </p>
+      <hr className="my-4" />
+
+      <PostArticle content={historyContent.htmlContent} />
+    </section>
+  );
+}
