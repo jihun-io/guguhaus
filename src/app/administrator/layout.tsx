18b698831aa177701e69b73e8366544a59e19503
김지훈 <kim@jihun.io>
2025-03-06 22:07:06 +0900

    관리자 페이지 관련 컴포넌트 및 API 파일 삭제

--- src/app/administrator/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AuthProvider from "@/auth/Provider";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
--- src/app/administrator/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/admin/LoginForm";
-
-export default function Login() {
-  return (
-    <>
-      <LoginForm />
-    </>
-  );
-}
