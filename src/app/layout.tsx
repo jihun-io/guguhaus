73c9c97607e53c77e7662b6dbbbc5cc699be7a44
김지훈 <kim@jihun.io>
2025-03-09 01:37:56 +0900

    글로벌 스타일에 나눔바른고딕 및 코레일 다이나믹 서브셋 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,35 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "../../public/fonts/NanumBarunGothic/NanumBarunGothic.css";
+import "../../public/fonts/NanumBarunGothicBold/NanumBarunGothicBold.css";
+import "../../public/fonts/NanumBarunGothicLight/NanumBarunGothicLight.css";
+import "../../public/fonts/NanumBarunGothicUltraLight/NanumBarunGothicUltraLight.css";
+import "../../public/fonts/korail_Condensed/korail_Condensed.css";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
+// const korailCondensed = localFont({
+//   src: "./fonts/KORAILCondensed-Regular.woff2",
+//   variable: "--font-korail-condensed",
 // });
 
-const korailCondensed = localFont({
-  src: "./fonts/KORAILCondensed-Regular.woff2",
-  variable: "--font-korail-condensed",
-});
-
-const nanumBarunGothic = localFont({
-  src: [
-    {
-      path: "./fonts/NanumBarunGothic.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/NanumBarunGothicBold.woff",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-nanum-barun-gothic",
-});
+// const nanumBarunGothic = localFont({
+//   src: [
+//     {
+//       path: "./fonts/NanumBarunGothic.woff",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/NanumBarunGothicBold.woff",
+//       weight: "700",
+//       style: "normal",
+//     },
+//   ],
+//   variable: "--font-nanum-barun-gothic",
+// });
 
 export const metadata: Metadata = {
   title: "99haus",
@@ -55,7 +50,7 @@ export default function RootLayout({
       </head>
       <body
         className={`px-8 min-h-[100dvh] grid grid-rows-[auto,1fr,auto] prose-p:text-foreground prose-headings:text-foreground overflow-y-scroll
-          ${nanumBarunGothic.variable} ${korailCondensed.variable} font-nanum-barun-gothic
+          font-nanum-barun-gothic
           `}
       >
         <Header />
