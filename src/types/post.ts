ede7a165fd858f13a525c97a6221bc00b111cc99
김지훈 <kim@jihun.io>
2025-05-14 22:31:30 +0900

    WIP, Article, History, Participants 타입 정의

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,20 +1,122 @@
+export interface WipItem {
+  id: string;
+  postId: string;
+  title: string;
+  year: string;
+  genre: string;
+  image: string;
+  imageAlt: string;
+}
+
+export interface WipDb {
+  thumbnail: string;
+  id: string;
+  postid: string;
+  title: string;
+  year: string;
+  genre: string;
+  imagealt: string;
+}
+
+export interface ArticleItem {
+  id: string;
+  postId: string;
+  title: string;
+  desc: string;
+  articleCategory: string;
+  image: string;
+  imageAlt: string;
+}
+export interface ArticleDb {
+  id: string;
+  postid: string;
+  title: string;
+  desc: string;
+  articlecategory: string;
+  thumbnail: string;
+  imagealt: string;
+}
+
+export interface HistoryItem {
+  id: string;
+  postId: string;
+  title: string;
+  titleEng: string;
+  year: string;
+  historyCategory: string;
+  image: string;
+  imageAlt: string;
+}
+export interface HistoryDb {
+  id: string;
+  postid: string;
+  title: string;
+  titleeng: string;
+  year: string;
+  historycategory: string;
+  thumbnail: string;
+  imagealt: string;
+}
+
+export interface ParticipantsItem {
+  id: string;
+  artist: string;
+  job: string;
+  image: string;
+  imageAlt: string;
+  href: string;
+}
+export interface ParticipantsDb {
+  id: string;
+  artist: string;
+  job: string;
+  image: string;
+  imagealt: string;
+  href: string;
+}
+
 export interface PostTypes {
+  properties: {
+    id?: string;
+    title: string;
+    postId: string;
+    category: "Work In Progress" | "Articles" | "History";
+    createdAt?: string;
+    content: string;
+    updatedAt: string;
+    thumbnail?: string;
+    imageUrl?: string;
+    imageAlt: string;
+    // 기타 필드
+    date?: string;
+    year?: string;
+    genre?: string;
+    articleCategory?: string;
+    historyCategory?: string;
+    desc?: string;
+    titleEng?: string;
+    isDeleted?: boolean;
+    createdTime: string;
+  };
+  htmlContent: string;
+}
+
+export interface PostDb {
   id?: string;
   title: string;
-  postId: string;
-  category: "Work In Progress" | "Articles" | "History";
-  createdAt?: string;
-  content: string;
-  updatedAt: string;
-  image?: string;
-  imageUrl?: string;
-  imageAlt: string;
-  // 기타 필드
+  postid: string;
+  created_at?: string;
+  htmlcontent: string;
+  updatedat: string;
+  thumbnail?: string;
+  imageurl?: string;
+  imagealt: string;
   date?: string;
+  year?: string;
   genre?: string;
-  articleCategory?: string;
-  historyCategory?: string;
+  articlecategory?: string;
+  historycategory?: string;
   desc?: string;
-  titleEng?: string;
-  isDeleted?: boolean;
+  titleeng?: string;
+  isdeleted?: boolean;
 }
