f70820094c41b01aaa75d5bb96af9d4539ff2af5
김지훈 <kim@jihun.io>
2025-06-21 21:16:50 +0900

    연락처 이메일 주소를 업데이트

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,8 +12,8 @@ export default function Contact() {
     <>
       <MainTitle title="CONTACT" subtitle="연락" />
       <article className="text-center h-full flex flex-col justify-center items-center">
-        <a href="mailto:saeparandol@naver.com" className="block my-8">
-          saeparandol@naver.com
+        <a href="mailto:greenplanet1415@gmail.com" className="block my-8">
+          greenplanet1415@gmail.com
         </a>
         <p>특별한 문의 사항이 있을 시 위의 이메일로 연락 부탁드리겠습니다.</p>
         <p className="uppercase text-sm">
