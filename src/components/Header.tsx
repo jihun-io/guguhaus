b0968bccb15bdaaa70a15bd4ae4636d26ed3fc68
김지훈 <kim@jihun.io>
2025-11-16 22:04:12 +0900

    헤더에 로고를 클릭할 수 있는 링크 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="flex flex-col items-center justify-center w-full my-6">
-      <h1>
-        <Image src="/images/Logo.svg" width={120} height={100} alt="99haus" />
-      </h1>
+      <a href="/">
+        <h1>
+          <Image src="/images/Logo.svg" width={120} height={100} alt="99haus" />
+        </h1>
+      </a>
       <p className="font-bold text-sm">Live. Laugh. Love.</p>
     </header>
   );
