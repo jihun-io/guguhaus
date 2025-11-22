f54184df0733c720de7cdf8154ffb240ab155530
김지훈 <kim@jihun.io>
2025-11-23 05:57:12 +0900

    개행 문자 처리

--- src/app/history/[postId]/page.tsx
+++ src/app/history/[postId]/page.tsx
@@ -34,7 +34,7 @@ export default async function HistoryPage({
         </div>
         <hr className="border border-hr my-8" />
         <div
-          className="prose max-w-full prose-li:prose-p:my-0 prose-h4:text-2xl"
+          className="prose max-w-full prose-li:prose-p:my-0 prose-h4:text-2xl prose-p:whitespace-pre-wrap"
           dangerouslySetInnerHTML={{ __html: data.htmlContent }}
         ></div>
       </article>
