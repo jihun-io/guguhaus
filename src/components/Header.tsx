fed3a6e00455529bf26ad302f974b69b55de043b
김지훈 <kim@jihun.io>
2025-11-22 23:30:43 +0900

    헤더에 내비게이션 추가 및 로고 링크에 p 태그 포함

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -7,8 +8,24 @@ export default function Header() {
         <h1>
           <Image src="/images/Logo.svg" width={120} height={100} alt="99haus" />
         </h1>
+        <p className="font-bold text-sm pb-6">Live. Laugh. Love.</p>
       </a>
-      <p className="font-bold text-sm">Live. Laugh. Love.</p>
+      <nav className="w-full">
+        <ul className="grid grid-cols-3 justify-centert justify-between font-bold">
+          <li
+            aria-label="Originals"
+            className="text-xs sm:text-[1em] text-center uppercase"
+          >
+            <Link href="/originals">Ori9inals</Link>
+          </li>
+          <li className="text-xs sm:text-[1em] text-center uppercase">
+            <Link href="/merch">Merchandise</Link>
+          </li>
+          <li className="text-xs sm:text-[1em] text-center uppercase">
+            <Link href="/participants">Participants</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
