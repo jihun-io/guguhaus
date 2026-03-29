b0e3ea9fccfe99a18bb8cee35b3f3d84b42e09d4
김지훈 <kim@jihun.io>
2026-03-29 12:18:09 +0900

    edit: PostgreSQL 연결 방식 변경

--- src/lib/postgres.ts
+++ src/lib/postgres.ts
@@ -1,15 +1,11 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 
-const client = new Client({
+const client = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: Number(process.env.PG_PORT),
-});
-
-client.connect().catch((err) => {
-  console.error("PostgreSQL 연결에 실패했습니다:", err);
 });
 
 export default client;
